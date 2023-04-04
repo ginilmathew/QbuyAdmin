182b437972467c244b1a92c7f220ce6a9efcd538
Amal Babu <amal@diginestsolutions.in>
2023-04-04 17:49:57 +0530

    'update'

--- src/components/CustomTimepicker.tsx
+++ src/components/CustomTimepicker.tsx
@@ -32,7 +32,7 @@ const CustomTimepicker = ({
                 <Typography letterSpacing={1} px={'3px'} mb={'3px'}
                     sx={{
                         fontSize: {
-                            lg: 18,
+                            lg: 16,
                             md: 14,
                             sm: 12,
                             xs: 11,
