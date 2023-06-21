a612d8e4b5b474f5f878ab3f5daeac3f65bf3bd6
Renjithpaul <renjithpaul@192.168.1.2>
2023-06-21 15:47:21 +0530

    update

--- src/Config/index.tsx
+++ src/Config/index.tsx
@@ -5,6 +5,7 @@
 // 1- corporate, 2-ta, 3-customer, 4 -supplier
 
 export const env = "dev"
+
 const URLS = {
     live: "https://apiqbuygreen.diginestsolutions.in/public/api/",
     testing: "https://qbuygreenapi.diginestsolutions.in/public/api/",
