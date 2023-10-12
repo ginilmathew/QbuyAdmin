6faa3b5d500c1f19fdab02d20ac9c530cedab970
ginil <ginil@diginestsolutions.in>
2023-10-12 11:20:02 +0530

    update

--- src/Widgets/Product/productForm.tsx
+++ src/Widgets/Product/productForm.tsx
@@ -1417,7 +1417,7 @@ const ProductForm = ({ res, view }: props) => {
                     </Grid>
 
 
-                    {/* <Grid item xs={12} lg={3}>
+                    <Grid item xs={12} lg={3}>
                         <CustomMultiselect
 
                             multiple={true}
@@ -1438,9 +1438,9 @@ const ProductForm = ({ res, view }: props) => {
                                 <MenuItem value={res?.value}>{res?.name}</MenuItem>
                             ))}
                         </CustomMultiselect>
-                    </Grid> */}
+                    </Grid> 
 
-                    {/* <Grid item xs={12} lg={3}>
+                     <Grid item xs={12} lg={3}>
                         <CustomMultiselect
 
                             multiple={true}
@@ -1461,8 +1461,8 @@ const ProductForm = ({ res, view }: props) => {
                                 <MenuItem key={res?._id} value={res?._id}>{res?.name}</MenuItem>
                             ))}
                         </CustomMultiselect>
-                    </Grid> */}
-{/* 
+                    </Grid> 
+
                     <Grid item xs={12} lg={3}>
                         <Customselect
                             type='text'
@@ -1487,7 +1487,7 @@ const ProductForm = ({ res, view }: props) => {
                             ))}
 
                         </Customselect>
-                    </Grid> */}
+                    </Grid>
 
 
                     <Grid item xs={12} lg={1.5}>
